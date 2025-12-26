96008c8b665719b27c808b797e5fd7e7fe51ded0
student <student@corp.kpiot.ru>
2025-12-26 11:07:18 +0500

    .

--- student_solution.py
+++ student_solution.py
@@ -5,26 +5,48 @@ def task1(s):
     # s — строка вида "подстрока1,подстрока2"
     # вернуть кортеж: (len(sub1) > len(sub2), sub1==sub2, sub2 in sub1)
     ...
-
+s=input()
+a,b=s.split(',',1)
+a=a.strip()
+b=b.strip()
+print(len(a)>len(b))
+print(a==b)
+print(b in a)
 # ---------- ЗАДАНИЕ 2 ----------
 def task2(s):
     # s — любая строка
     # вернуть кортеж:
     # (s.strip(), len(s), s.count('a'), s.replace('a','@'), s.istitle())
     ...
-
+s=input()
+t=s.strip()
+print(t)
+print(len(t))
+print(t.count('a'))
+print(t.replace('a','@'))
+print(t and t[0].isupper())
 # ---------- ЗАДАНИЕ 3 ----------
 def task3(s):
     # s — строка
     # вернуть кортеж: (без первого и последнего символа, каждый второй символ, строка.lower() в обратном порядке)
     ...
-
+input_string=input()
+a=down_tab_shift=input_string[1:-1]
+b=down_tab_shift=input_string[::2]
+c=down_tab_shift=input_string[::-1]
+print(a)
+print(b)
+print(c)
 # ---------- ЗАДАНИЕ 4 ----------
 def task4(nums):
     # nums — список чисел
     # вернуть кортеж: (отсортированный список, сумма, (min, max))
     ...
-
+s=input().strip()
+nums=list(map(int,s.split()))
+print(sorted(nums))
+print(sum(nums))
+print(min(nums),max(nums))
 
 # ---------- ЗАДАНИЕ 5 ----------
 def task6(s):
